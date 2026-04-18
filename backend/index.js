f84ed400683f4c0c6443571f0ce7e3126fe81a6d
alammd0 <sonualam96314@gmail.com>
2026-04-18 17:13:10 +0530

    complete target of this project

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,11 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import userRoutes from './routes/user.routes.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
